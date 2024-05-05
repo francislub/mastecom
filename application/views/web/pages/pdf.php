5550029e88fb3a64002e097e045f5f80a37e0f90
francislub <lubanjwafrancis@gmail.com>
2024-05-05 16:00:49 +0300

    initails

---
+++ application/views/web/pages/pdf.php
@@ -0,0 +1,12 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+	<meta charset="UTF-8">
+	<title>Dompdf Practise</title>
+</head>
+<body>
+	<h1>Hello World</h1>
+	<img src="" alt="" />
+	<p>Copyright</p>
+</body>
+</html>
